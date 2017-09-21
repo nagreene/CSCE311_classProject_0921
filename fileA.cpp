78f764bf2561f2bc5dec39108600e96554384351
LoganFollett <32179215+LoganFollett@users.noreply.github.com>
2017-09-21 13:45:06 -0800

    Add files via upload

--- fileA.cpp
+++ fileA.cpp
@@ -5,9 +5,12 @@ string fooA(string inpString)
 {
 	if(inpString == "")
 		return "";
-	return fooA(inpString.substr(0, inpString.length() - 1)) + inpString.substr(inpString.length() - 1) + "!";
+	return fooA(inpString.substr(0, inpString.length() - 1))//Most of inpString, filtered through fooA
+		+ inpString.substr(inpString.length() - 1)//Last char of inpString
+		+ "!";//Exclamation point
 }
 int main()
 {
 	cout << fooZ("Foo Bar Zot") << endl;
+	system("pause");
 }
